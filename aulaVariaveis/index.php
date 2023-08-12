9f1f5f1ca00c99a7315c89ce106136e876402e83
JairoJJr <jairojannini.contato@gmail.com>
2023-08-12 17:39:45 -0300

    aula de variáveis

--- aulaVariaveis/index.php
+++ aulaVariaveis/index.php
@@ -1,4 +1,15 @@
-<?php 
+
+<!DOCTYPE html>
+<html lang="pt=br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Aula Variáveis</title>
+</head>
+<body>
+    <a href= "index2.php">outro código</a>
+
+    <?php 
     $nome = "Jairo";
     $sobrenome = "Junior";
     const PAIS = "Brasil";
@@ -12,4 +23,9 @@
 
 
     echo "Muito prazer, $nome $sobrenome! Você mora no " . PAIS;
+    $nomeCompletoCliente = "Camel Case";
+    $nome_completo_cliente = "Snake Case";
+    $nomecompletocliente = "";
 ?>
+</body>
+</html>
---
+++ aulaVariaveis/index2.php
@@ -0,0 +1,5 @@
+<?php 
+    $nome = "Pedro";
+
+    echo "É verdade que seu nome é $nome?";
+?>
