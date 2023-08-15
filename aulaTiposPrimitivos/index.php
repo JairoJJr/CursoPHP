7ee3c6b28869d917831d2e4fe48fbdc9b95f9d68
JairoJJr <jairojannini.contato@gmail.com>
2023-08-15 16:20:11 -0300

    Aula Tipos Primitivos

---
+++ aulaTiposPrimitivos/index.php
@@ -0,0 +1,22 @@
+<?php 
+    //Tipos primitivos escalares
+
+    $sobrenome = "Silva"; // String (cadeia de caracteres)
+    $idade = 34; // int ou integer
+    $peso = 85.9; // float, double ou real (real não existe mais)
+    $casado = true; // Booleana (bool)
+
+    //  números hexadecimais começam com 0x em PHP
+    //  números representados como potência (3e2) são float
+
+    //Tipos primitivos compostos 
+    // array
+    // object
+
+    //Tipos primitivos especiais
+    // null
+    // resource
+    // callabe
+    // mixed (todos)
+
+?>
