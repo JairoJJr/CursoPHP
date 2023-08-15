82dbf0224103044225589bbee87f3d59e1f34ebf
JairoJJr <jairojannini.contato@gmail.com>
2023-08-15 20:26:19 -0300

    primeira parte aula de Strings

---
+++ aulaStrings/index.php
@@ -0,0 +1,12 @@
+<?php 
+    //double quoted
+        // "Curso" 
+        echo "PHP\u{1F418}" ."<br>";     
+    //single quoted
+        // 'Curso'
+        echo 'PHP\u\[1F418}' . "<br>"
+    //Heredoc
+    //Nowdoc
+
+    
+?>
---
+++ ex004/index.php
@@ -0,0 +1,10 @@
+<?php 
+    $nome = "Jairo";
+    $sobrenome = "Jannini Junior";
+    echo "$nome $sobrenome\u{1F595}";
+    echo "<br>";
+    echo '$nome $sobrenome\u{1F595}' . "<br>";
+
+    const CANAL = "Curso em Vídeo \u{1F595}";
+    echo "Eu adoro o " . CANAL;
+?>
