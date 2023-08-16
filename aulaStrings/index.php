066ad462305ee2249036326945893d02af266e19
JairoJJr <jairojannini.contato@gmail.com>
2023-08-16 20:40:58 -0300

    final da aula de strings

--- aulaStrings/index.php
+++ aulaStrings/index.php
@@ -4,9 +4,19 @@
         echo "PHP\u{1F418}" ."<br>";     
     //single quoted
         // 'Curso'
-        echo 'PHP\u\[1F418}' . "<br>"
+        echo 'PHP\u\[1F418}' . "<br>";
     //Heredoc
-    //Nowdoc
+        $canal = "Curso em Vídeo";
+        $ano = date("Y");
+        echo <<< FRASE
+            Olá Eu do Futuro,
+                    Você já aprendeu PHP?
+                Você começou com o $canal
+                Lembra? Em 2023?
+                Agora já é $ano
+
+        FRASE;
+    //Nowdoc- não contém interpretação
 
     
 ?>
--- ex004/index.php
+++ ex004/index.php
@@ -7,4 +7,13 @@
 
     const CANAL = "Curso em Vídeo \u{1F595}";
     echo "Eu adoro o " . CANAL;
+
+    echo "<br>Estamos no ano " . date("Y") . "<br>";
+
+    $nom = "Rodrigo";
+    $sobrenome = "Nogueira";
+    echo "$nom \"Minotauro\" $sobrenome"; 
+    //Sequências de escapes:   \n - Nova linha   \t - Tabulação horizontal   \\ Barra invertida  \$ Sinal de cifrão \u{} Codepoint Unicode
+
+    
 ?>
